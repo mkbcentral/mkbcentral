472d2291b5f28d647eb6c5f98f8e39a3773bc9e4
mkbcentral <mkbcentralcode@gmail.com>
2022-07-13 15:46:44 +0200

    Change assets

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,9 +10,8 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-        <script src="https://cdn.tailwindcss.com"></script>
-
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
